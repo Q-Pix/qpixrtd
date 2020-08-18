29736248e4e329ad6a32a7e0f65bad72d6f976c6
AustinMcDonald <austin.mcdonald96@mavs.uta.edu>
2020-08-18 15:37:50 -0500

    added diffuser to root reader

--- root_read_test/root_macro.cpp
+++ root_read_test/root_macro.cpp
@@ -18,8 +18,11 @@
 // #include "TFile.h"
 // #include "TROOT.h"
 
-
+// Qpix includes
 #include "Qpix/ReadG4root.h"
+
+#include <ctime>
+
 
 
 //----------------------------------------------------------------------
@@ -27,16 +30,51 @@
 //----------------------------------------------------------------------
 int main()
 {
+  clock_t time_req;
+  time_req = clock();
 
   std::vector< std::string > file_list_ = 
   {
     "/Users/austinmcdonald/projects/Q_PIX_NEW/Q_PIX_RTD/root_read_test/test.root",
     "/Users/austinmcdonald/projects/Q_PIX_NEW/Q_PIX_RTD/root_read_test/test2.root"
   };
+
+
+  Qpix::Liquid_Argon_Paramaters * LAr_params = new Qpix::Liquid_Argon_Paramaters();
+  set_Liquid_Argon_Paramaters(LAr_params);
+  print_Liquid_Argon_Paramaters(LAr_params);
+
+
   int evt=1;
-  Qpix::ROOT_READ(file_list_, evt);
+  //std::vector<double> hit_e;
+  std::vector<Qpix::ELECTRON> hit_e;
+  Qpix::ROOT_READ(file_list_, evt, LAr_params, hit_e);
 
 
-  //std::cout << "size of hit_end_x_ = " << hit_end_x_.size() << std::endl;
-  return 0;
+
+  std::cout << "size of hit_e = " << hit_e.size() << std::endl;
+
+
+
+  // for (int i = 0; i < hit_e.size(); i++) 
+  // {
+  //   std::cout << hit_e[i].x_pos << "\t"
+  //             << hit_e[i].y_pos << "\t"
+  //             << hit_e[i].z_pos << "\t"
+  //             << hit_e[i].t_pos << "\t"
+  //             << std::endl;
+
+  // }
+
+  
+
+
+
+  std::cout << "done" << std::endl;
+
+    time_req = clock() - time_req;
+    double time = (float)time_req/CLOCKS_PER_SEC;
+    std::cout<< "The operation took "<<time<<" Seconds"<<std::endl;
+    return 0;
+
 } // main()
