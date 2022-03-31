2be70f3b2892aafa2a57baf33dea6e0bb3a971a9
Dave Elofson <dave.elofson@tamu.edu>
2022-03-31 16:10:21 -0500

    Breaking setup.sh into 2 files and improving efficiency

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+###########################################
+# build.sh
+#
+# Author: Dave Elofson
+# Created: 2022-03-31
+##########################################
+
+# This script will make the build directory, then run cmake and make. Should only have to be run the first time after installation
+
+
+source ./setup.sh
+
+mkdir Build
+cd Build
+cmake ../
+make
+
+# Moving the lib
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    cp ./source/libQPixRTD.so ../Library/.
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    cp ./source/libQPixRTD.dylib ../Library/.
+fi
+
+cp ./source/libQPixRTDDict_rdict.pcm ../Library/.
+cp ./source/libQPixRTDDict.rootmap ../Library/.
+
+
--- setup.sh
+++ setup.sh
@@ -1,49 +1,5 @@
 #!/bin/sh
-printf '************************************* \n'
-message="Hello human I will now set up your Qpix enviorment."; for ((i=0; i<${#message}; i++)); do echo "after 75" | tclsh; printf "${message:$i:1}"; done; echo;
-sleep 1
-
 # setting up the Qpix path
-printf '************************************* \n'
 export QPixRTDDir=$PWD
-printf 'Defined your Qpix path as \n'
-echo $QPixRTDDir
-sleep 1
-
-
 cd $QPixRTDDir
-
-printf '************************************* \n'
-printf 'Make/goto the Build folder and compile \n'
-printf '\n'
-sleep 1
-
-# Making the build directory
-mkdir Build
-cd Build
-cmake ../
-make
-
-sleep 1
-printf '\n'
-printf '************************************* \n'
-printf 'Moving the joint library \n'
-
-# Moving the lib
-
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    cp ./source/libQPixRTD.so ../Library/.
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    cp ./source/libQPixRTD.dylib ../Library/.
-fi
-
-cp ./source/libQPixRTDDict_rdict.pcm ../Library/.
-cp ./source/libQPixRTDDict.rootmap ../Library/.
-
-cd $QPixRTDDir
-printf '************************************* \n'
-sleep 1
-
-message="Human you may get to work now."; for ((i=0; i<${#message}; i++)); do echo "after 75" | tclsh; printf "${message:$i:1}"; done; echo;
-
 
