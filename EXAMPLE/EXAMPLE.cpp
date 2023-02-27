6275cbb975546a4ba32fcad12f37549197fed0f0
AustinMcDonald <austin.mcdonald96@mavs.uta.edu>
2023-02-27 16:14:56 -0600

    measuring timing towards performance

--- EXAMPLE/EXAMPLE.cpp
+++ EXAMPLE/EXAMPLE.cpp
@@ -11,13 +11,25 @@
 #include "Structures.h"
 #include "PixelResponse.h"
 
+void avg(std::vector<clock_t> start_times, std::vector<clock_t> stop_times, std::string msg){
+  int size = start_times.size();
+  double avgs;
+  for(int i=0; i<start_times.size(); ++i){
+    clock_t time_req = stop_times[i] - start_times[i];
+    avgs += (float)time_req/CLOCKS_PER_SEC;
+  }
+
+  std::cout << msg << avgs/size << std::endl;
+}
+
+
 //----------------------------------------------------------------------
 // main function
 //----------------------------------------------------------------------
 int main(int argc, char** argv)
 {
   // changing the seed for the random numbergenerator and generating the noise vector 
-  constexpr std::uint64_t Seed = 777;
+  constexpr std::uint64_t Seed = 41;
   Qpix::Random_Set_Seed(Seed);
   std::vector<double> Gaussian_Noise = Qpix::Make_Gaussian_Noise(0, (int) 1e7);
 
@@ -46,37 +58,55 @@ int main(int argc, char** argv)
   time_req = clock();
   double time;
 
+  std::vector<clock_t> start_pixelize_times, start_reset_times;
+  std::vector<clock_t> stop_pixelize_times, stop_reset_times;
+
   // Loop though the events in the file
   for (int evt = 0; evt < number_entries; evt++)
   {
     // std::cout << "*********************************************" << std::endl;
     // std::cout << "Starting on event " << evt << std::endl;
 
-    if(evt%100 == 0)
-      std::cout << "Getting the event" << std::endl;
+    if(evt%1000 == 0){
+      std::cout << "Getting the event: " << evt << std::endl;
+      avg(start_pixelize_times, stop_pixelize_times, "pixel times average: ");
+      avg(start_reset_times, stop_reset_times, "reset times average: ");
+      start_reset_times.clear();
+      stop_reset_times.clear();
+      start_pixelize_times.clear();
+      stop_pixelize_times.clear();
+    }
     // turn the Geant4 hits into electrons
     std::vector<Qpix::ELECTRON> hit_e;
-    rfm.Get_Event(evt, Qpix_params, hit_e, false);
+    // rfm.Get_Event(evt, Qpix_params, hit_e, true); // sort by index
+    rfm.Get_Event(evt, Qpix_params, hit_e, false); // sort by time, not index
     // std::cout << "size of hit_e = " << hit_e.size() << std::endl;
 
     // Pixelize the electrons 
     // std::cout << "Pixelizing the event" << std::endl;
     Qpix::Pixel_Functions PixFunc = Qpix::Pixel_Functions();
-    // std::vector<Qpix::Pixel_Info> Pixel;
-    // PixFunc.Pixelize_Event(hit_e, Pixel);
+    std::vector<Qpix::Pixel_Info> Pixel;
 
+    start_pixelize_times.push_back(clock());
+    // PixFunc.Pixelize_Event(hit_e, Pixel);
     std::set<int> hit_pixels = PixFunc.Pixelize_Event(hit_e, mPixelInfo);
+    stop_pixelize_times.push_back(clock());
+
+    // for(int hid : hit_pixels)
+    //   Pixel.push_back(mPixelInfo[hid]);
 
     // std::cout << "Running the resets" << std::endl;
     // the reset function
-    PixFunc.Reset_Fast(Qpix_params, Gaussian_Noise, hit_pixels, mPixelInfo);
+    start_reset_times.push_back(clock());
     // PixFunc.Reset_Fast(Qpix_params, Gaussian_Noise, Pixel);
+    PixFunc.Reset_Fast(Qpix_params, Gaussian_Noise, hit_pixels, mPixelInfo);
+    stop_reset_times.push_back(clock());
 
     // rfm.AddEvent(Pixel);
     rfm.AddEvent(hit_pixels, mPixelInfo);
 
     rfm.EventFill();
-    // rfm.EventReset();
+    rfm.EventReset();
   }
 
   // save and close
